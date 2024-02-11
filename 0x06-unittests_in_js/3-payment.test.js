9b6fa777e6a965901270f5d0a898a0c9548c7986
Okpah Evans Chukwunnaemeka <okpahevans@gmail.com>
2024-02-11 09:57:40 -0800

    Update 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,12 +5,12 @@ const Utils = require("./utils");
 const assert = require("assert");
 
 describe("sendPaymentRequestToApi", function() {
-	    it("check that Utils.calculateNumber was called once", function() {
-		    	const spy = sinon.spy(Utils, "calculateNumber");
+    it("check that Utils.calculateNumber was called once", function() {
+	const spy = sinon.spy(Utils, "calculateNumber");
 
-		    	sendPaymentRequestToApi(50, 24.52);
+	sendPaymentRequestToApi(50, 24.52);
 
-		    	assert(spy.calledOnce);
-		    	spy.restore();
-		        });
+	assert(spy.calledOnce);
+	spy.restore();
+    });
 });
