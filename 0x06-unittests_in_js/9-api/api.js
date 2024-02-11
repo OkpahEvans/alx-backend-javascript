80386e19c182c7d2ea5861e59409b4712be6fa45
Okpah Evans Chukwunnaemeka <okpahevans@gmail.com>
2024-02-11 10:13:44 -0800

    Update api.js

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -4,15 +4,15 @@ const app = express();
 const port = 7865;
 
 app.get('/', (request, response) => {
-	  response.send('Welcome to the payment system');
+  response.send('Welcome to the payment system');
 });
 
 app.get('/cart/:id([0-9]+)', (request, response) => {
-	    response.send(`Payment methods for cart ${request.params.id}`);
+    response.send(`Payment methods for cart ${request.params.id}`);
 });
 
 app.listen(port, () => {
-	    console.log(`API available on localhost port ${port}`);
+    console.log(`API available on localhost port ${port}`);
 });
 
 module.exports = app;
