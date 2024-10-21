3abd08c8a6a9f339d1bac62a0f5dba8073c8906a
Okpah Evans Chukwunnaemeka <okpahevans@gmail.com>
2024-10-21 13:34:55 -0700

    Update 5-payment.test.js

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -4,20 +4,20 @@ const sendPaymentRequestToApi = require("./5-payment");
 const expect = require("chai").expect;
 
 describe("sendPaymentRequestToApi", function() {
-	    beforeEach("Set up spy to use for each test", function() {
-		    	sinon.spy(console, "log");
-		        });
-	    afterEach("restore spy after each test", function() {
-		    	console.log.restore();
-		        });
-	    it("check that console.log is called with the right arg", function() {
-		    	sendPaymentRequestToApi(100, 20);
+    beforeEach("Set up spy to use for each test", function() {
+	sinon.spy(console, "log");
+    });
+    afterEach("restore spy after each test", function() {
+	console.log.restore();
+    });
+    it("check that console.log is called with the right arg", function() {
+	sendPaymentRequestToApi(100, 20);
 
-		    	expect(console.log.withArgs("The total is: 120").calledOnce).to.be.true;
-		        });
-	    it("check that console.log is called with the right arg", function() {
-		    	sendPaymentRequestToApi(10, 10);
+	expect(console.log.withArgs("The total is: 120").calledOnce).to.be.true;
+    });
+    it("check that console.log is called with the right arg", function() {
+	sendPaymentRequestToApi(10, 10);
 
-		    	expect(console.log.withArgs("The total is: 20").calledOnce).to.be.true;
-		        });
+	expect(console.log.withArgs("The total is: 20").calledOnce).to.be.true;
+    });
 });
