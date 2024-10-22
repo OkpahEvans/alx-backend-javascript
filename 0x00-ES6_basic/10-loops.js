2c070161aaf82acc186e76a7551a9d6bb92583dc
Okpah Evans Chukwunnaemeka <okpahevans@gmail.com>
2024-10-22 07:32:52 -0700

    Update 10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const arrayEnd = [];
-  for (const idx of array) {
-    arrayEnd.push(`${appendString}${idx}`);
+  for (const value of array) {
+    const idx = array.indexOf(value);
+    array[idx] = appendString + value; // eslint-disable-line no-param-reassign
   }
 
-  return arrayEnd;
+  return array;
 }
